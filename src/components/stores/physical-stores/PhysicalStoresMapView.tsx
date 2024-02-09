0abdb579001d80c1bd5faaff2c9f1fd0f5b16e11
sarju9 <sarjusejpal3953@gmail.com>
2024-02-09 20:29:24 +0530

    map integration done

--- src/components/stores/physical-stores/PhysicalStoresMapView.tsx
+++ src/components/stores/physical-stores/PhysicalStoresMapView.tsx
@@ -1,14 +1,84 @@
-import { Grid } from '@mui/material'
-import React from 'react'
+import React, { useCallback } from 'react'
+import { GoogleMap, useJsApiLoader, MarkerF } from '@react-google-maps/api';
 
-const PhysicalStoresMapView = () => {
-    return (
-        <>
-            <Grid item lg={8} md={7} sm={12} xs={12}>
-            Map
-            </Grid>
-        </>
-    )
+const containerStyle = {
+    width: '100%',
+    height: '100%'
+};
+
+// const center = {
+//     lat: 22.308155,
+//     lng: 70.800705,
+// };
+
+const markers = [
+    {
+        id: 1,
+        position: {
+            lat: 22.811989,
+            lng: 70.823616,
+        },
+    },
+    {
+        id: 2,
+        position: {
+            lat: 22.308155,
+            lng: 70.800705,
+        },
+    },
+]
+
+const mapCenter = {
+    lat: 22.811989,
+    lng: 70.823616,
 }
 
-export default PhysicalStoresMapView
+function MyComponent() {
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyAV4oc8VL6RXlw64B3x16JMqKqaJitu40Q",
+        version: "quaterly",
+    })
+
+    const [map, setMap] = React.useState(null)
+
+    const onLoad = React.useCallback(function callback(map) {
+        // This is just an example of getting and using the map instance!!! don't just blindly copy!
+        // const bounds = new window.google.maps.LatLngBounds(mapCenter);
+        // map.fitBounds(bounds);
+        // setMap(map)
+
+        map.setZoom(7)
+    }, [])
+
+    const onUnmount = useCallback(function callback(map) {
+        setMap(null)
+    }, [])
+
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={mapCenter}
+            zoom={13}
+            onLoad={onLoad}
+        // onUnmount={onUnmount}
+        >
+            { /* Child components, such as markers, info windows, etc. */}
+            {
+                markers.map((item) => {
+                    return (
+                        <MarkerF
+                            key={item?.id}
+                            position={item?.position}
+                        />
+                    )
+                })
+            }
+
+
+
+        </GoogleMap>
+    ) : <></>
+}
+
+export default MyComponent
